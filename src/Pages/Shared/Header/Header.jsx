8f4267adad71aadf64f96c63fbfa37ad69957618
rockydey <coderrocky25@gmail.com>
2022-04-16 22:38:46 +0600

    header section complete

--- src/Pages/Shared/Header/Header.jsx
+++ src/Pages/Shared/Header/Header.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Header.css';
 
 const Header = () => {
     return (
-        <div>
-            <h1>This is Header</h1>
-        </div>
+        <header className='bg-dark py-4'>
+            <div className='container d-flex justify-content-between align-items-center'>
+                <div className="logo-container">
+                    <h3 className='text-white'>DAVIS PHOTOGRAPHY</h3>
+                </div>
+                <div className="navbar-container">
+                    <Link className='me-4' to='/home'>Home</Link>
+                    <Link className='me-4' to='/about'>About Me</Link>
+                    <Link className='me-4' to='/blogs'>Blogs</Link>
+                    <Link className='me-4' to='/contact'>Contact</Link>
+                    <Link to='/login'><button className='border-0 rounded-pill btn-login text-white'>Login</button></Link>
+                </div>
+            </div>
+        </header>
     );
 };
 
